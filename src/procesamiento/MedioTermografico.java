5cfffcd75a7e2b19a35a4aecd6b59f0c1b86bac9
IzaccBelmont <jisagiizacc@gmail.com>
2019-04-30 11:25:22 -0500

    Correcion FabroMan Agragar mas elementos a la clase abstracta

--- src/procesamiento/MedioTermografico.java
+++ src/procesamiento/MedioTermografico.java
@@ -5,11 +5,60 @@
  */
 package procesamiento;
 
+import java.util.ArrayList;
+import modelos.Coordenada;
+import modelos.Estadistica;
+
 /**
  *
  * @author izacc
  */
 public abstract class MedioTermografico {
-    abstract void selecionPuntosInteres();
+    private String tipProceso;
+    private String fecha;
+    private String nombreProceso;
+    private String rutaImagenes;
+    private ArrayList<Coordenada> puntosInteres;
+    private Estadistica estadistica;
+
+    public MedioTermografico(String tipProceso, String fecha, String nombreProceso, 
+            String rutaImagenes, ArrayList<Coordenada> puntosInteres) {
+        this.tipProceso = tipProceso;
+        this.fecha = fecha;
+        this.nombreProceso = nombreProceso;
+        this.rutaImagenes = rutaImagenes;
+        this.puntosInteres = puntosInteres;
+        estadistica = null;
+    }
+    
+    abstract void agregarPuntosInteres(ArrayList<Coordenada> puntosInteres);
     abstract void calcularEstadistica();
+
+    public String getTipProceso() {
+        return tipProceso;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getNombreProceso() {
+        return nombreProceso;
+    }
+
+    public String getRutaImagenes() {
+        return rutaImagenes;
+    }
+
+    public ArrayList<Coordenada> getPuntosInteres() {
+        return puntosInteres;
+    }
+
+    public Estadistica getEstadistica() {
+        return estadistica;
+    }
+
+    public void setEstadistica(Estadistica estadistica) {
+        this.estadistica = estadistica;
+    }
 }
